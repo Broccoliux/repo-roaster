300b849d2e2fb7bdba8c41d0a72c0cbcfa7c3783
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-16 20:43:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,6 +6,18 @@ app = Flask(__name__)
 
 def home():
     return render_template("index.html")
+
+@app.route("/roast", methods=["POST"])
+def roast():
+    data = request.get_json()
+
+    repo_url = data.get("url", "")
+
+    return jsonify({
+        "success": True,
+        "message": "Backend received the URL!",
+        "url": repo_url
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
