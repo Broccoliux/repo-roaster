74841875b795e9b633283b8ba20217f7ca0dcecf
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-17 02:10:53 +0500

    Update github.py

--- github.py
+++ github.py
@@ -14,7 +14,9 @@ def fetch_repo_data(url):
     print(api_url)
     response = requests.get(api_url)
     print(response.url)
-    return response.status_code
+    
+    print("Status:", response.status_code)
+    print("Final URL:, respoonse.url")
+    print("Response:", response.text[:300])
 
-print(fetch_repo_data("https://github.com/torvalds/linix"))
-
+print(fetch_repo_data("https://github.com/torvalds/linux"))
