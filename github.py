ed8ae4cb607a218a128d8bb15e6a9d8e91c8c148
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-17 01:43:15 +0500

    Update github.py

---
+++ github.py
@@ -0,0 +1,21 @@
+import requests
+from urllib.parse import urlparse
+
+def extract_repo_info(url):
+    parsed = urlparse(url)
+    parts = parsed.path.strip("/").split("/")
+    return parts [0],parts [1]
+
+def fetch_repo_data(url):
+    owner, repo = extract_repo_info(url)
+    api_url = f"https://api.github.com/repos{owner}/{repo}"
+    print(owner)
+    print(repo)
+    print(api_url)
+    response = requests.get(api_url)
+    print(response.url)
+    return response.status_code
+
+print(fetch_repo_data("https://github.com/torvalds/linux"))
+
+
