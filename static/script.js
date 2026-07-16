bbc116d62340698297ec1f6fb8e9fa1cf16bd400
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-16 20:43:50 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,13 +1,35 @@
 const input = document.getElementById("repo-url");
 const button = document.getElementById("roast-btn");
 
-button.addEventListener("click", () => {
-    const url = input.vlaue.trim();
+button.addEventListener("click", async () => {
+    const url = input.value.trim();
 
     if (url === "") {
-        alert("Pleas enter a Github repositiry URL.");
+        alert("dont make a fuss u punk, drop ur sh**ty repo URL rn");
         return;
     }
 
-    console.log("Repository URL: ", url);
+    if (!isValidGitHubUrl(url)) {
+        alert("dont play with me kid, drop ur real sh*t");
+        return;
+    }
+
+    const response = await fetch("/roast", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            url: url
+        })
+    });
+
+    const data = await response.json();
+
+    console.log(data);
 });
+
+function isValidGitHubUrl(url) {
+    const pattern = /^https?:\/\/github\.com\/[^\/]+\/[^\/]+\/?$/;
+    return pattern.test(url);
+}
