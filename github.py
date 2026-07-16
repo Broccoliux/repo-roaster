0a9544d7e3db5ea65e4411e8ed9d37b737cb519f
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-17 02:03:32 +0500

    Update github.py

--- github.py
+++ github.py
@@ -8,7 +8,7 @@ def extract_repo_info(url):
 
 def fetch_repo_data(url):
     owner, repo = extract_repo_info(url)
-    api_url = f"https://api.github.com/repos{owner}/{repo}"
+    api_url = f"https://api.github.com/repos/{owner}/{repo}"
     print(owner)
     print(repo)
     print(api_url)
@@ -16,6 +16,5 @@ def fetch_repo_data(url):
     print(response.url)
     return response.status_code
 
-print(fetch_repo_data("https://github.com/torvalds/linux"))
-
+print(fetch_repo_data("https://github.com/torvalds/linix"))
 
