ecaa0384a335b037be9b4debc68e9ade44f27f19
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-17 12:29:35 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -31,7 +31,7 @@ button.addEventListener("click", async () => {
     result.innerHTML = `
     <h2>${data.repo.name}</h2>
     `;
-})
+});
 
 function isValidGitHubUrl(url) {
     const pattern = /^https?:\/\/github\.com\/[^\/]+\/[^\/]+\/?$/;
