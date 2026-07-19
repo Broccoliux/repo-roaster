df3c65f9e61cd6884eba74110c3cab0cfdf9172a
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-19 17:10:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 from github import fetch_repo_data, build_repo_context
 from roaster import roast_repo
+from cache import repo_cache
+
 
 import time
 import random
 
 app = Flask(__name__)
 
-# funny error messages shown to the user if Gemini fails
-
+# Funny error messages shown to the user if Gemini fails
 ERRORS = [
     "💀 Repo Roaster rage quit. Your repo dealt psychic damage to the AI. Try again in a minute, you freak.",
     "🤢 Your repo was so painful that Repo Roaster filed for workers' compensation.",
@@ -17,25 +18,25 @@ ERRORS = [
     "🤡 Your repo crashed the roaster before it could finish. I think you already know how cooked this repo is."
 ]
 
-# home page
 
+# Homepage
 @app.route("/")
 def home():
-    return render_template
+    return render_template("index.html")
 
 
-# roasting end point
-
+# Roast endpoint
 @app.route("/roast", methods=["POST"])
 def roast():
 
-    # get JSON send by JS\
+    # Get JSON sent from JavaScript
     data = request.get_json()
     repo_url = data.get("url", "")
 
-    # fetchs the repo info
+    # Fetch repository info
 
     start = time.time()
+
     repo = fetch_repo_data(repo_url)
 
     print("Repo:", time.time() - start)
@@ -46,43 +47,41 @@ def roast():
             "message": "Repository not found."
         }), 404
 
-    # build context for Gemini
+    # Build context for Gemini
 
     start = time.time()
- 
-    context = build_repo_context(repo_url)
-    print("Context:", time.time() - start)
 
-    # genration roast
+    if repo_url in repo_cache:
+
+        context = repo_cache[repo_url]
+        print("context: Loaded from cache")
+    else:
+        context = build_repo_context(repo_url)
+        repo_cache[repo_url] = context
+        print("conetxt: Built and cached")
+
+        print(f"Context: {time.time() - start:.3f}s")
+
+
+
+    # Generate roast
 
     start = time.time()
 
     try:
-        roast = roast_repo(context)
-
+        return Response(
+            roast_repo(context),
+            mimetype="text/plain"
+        )
+    
     except Exception as e:
-        
-        # Print the real  error in the terminal for debugging
-
         print("Gemini Error:", e)
-
-        # show funny message to the user
 
         return jsonify({
             "success": False,
             "message": random.choice(ERRORS)
         }), 500
-    
-    print("Gemini:", time.time() - start)
-
-    # send result back to frontend
-
-    return jsonify({
-        "success": True,
-        "repo": repo,
-        "roast": roast
-    })
 
 
-    if __name__ == "__main__":
-        app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
