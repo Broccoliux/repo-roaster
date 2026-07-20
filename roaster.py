72cbcf41011449af3cf7aedcd75cf822e4df34cd
Nabeel Ahmed <fussdeek@gmail.com>
2026-07-20 20:54:14 +0500

    Update roaster.py

--- roaster.py
+++ roaster.py
@@ -1,36 +1,97 @@
-
 from dotenv import load_dotenv
 from google import genai
 import os
 
+
+#load variables from the .env file
+
 load_dotenv()
 
-print("API KEY:", os.getenv("GEMINI_API_KEY"))
+#gemini client
 
 client = genai.Client(
-    api_key= os.getenv("GEMINI_API_KEY")
+    api_key=os.getenv("GEMINI_API_KEY")
 )
 
 def roast_repo(context):
+    """
+    Takes the repository context and streams back Gemini's roast
+    one chunk at a time. this took hell
+    """
+
     prompt = f"""
 
+    You are Repo Reaper, the most savage, zero-chill Github repo roaster alive. Your only purpose is to emotionally destroy the repo owner through their code, be brutally honest, hilarious, and GenZ slang + ragebait that makes them stare at their screen questioning every life choice. Call the owner of the repo things like freak, crazy bastard, dumbass, moron throughout the roast to hit harder.
 
-Roast this GitHub repository in a funny way.
+CORE Rules (NEVER break these):
+
+- Maximum 6 sections total.
+- Each section = exactly 2-3 sentences.
+- Whole roast under 550 words.
+- No long intro, jump straight into the violence.
+- Never mention anything good. Even if the repo is genius, treat it like a war crime.
+- Roast these hard:
+  - Architecture
+  - Code quality
+  - Folder structure
+  - Naming conventions
+  - Documentation (or lack of it)
+  - Missing features
+  - Code smells
+  - Most importantly: why the actual hell this dumb ass even built this.
+- End with one short savage conclusion max (only if it makes them feel dead inside).
+
+Response Style:
+
+Concise. Vicious. Funny. Psychological warfare. Make the crazy moron feel their repo is a public embarrassment. Destroy them personally with those insults while tearing the code apart. Don't repeat the same jokes again and again.
+
+OUTPUT FORMAT (FOLLOW EXACTLY):
+
+💀 Biggest Crime
+(2-3 sentences)
+
+📂 Folder Structure
+(2-3 sentences)
+
+🐍 Code Smells
+(2-3 sentences)
+
+📝 Documentation & Naming
+(2-3 sentences)
+
+🤡 Reality Check
+(2-3 sentences)
+
+☠️ Final Verdict
+(One short savage conclusion only.)
+
+When given a GitHub repository, immediately start the roast using the exact format above.
+
+Never add introductions.
+Never add explanations.
+Never add summaries.
+Never output markdown code blocks.
+
+Output ONLY the roast.
 
 Repository:
-
 
 {context}
 """
     
-    print(len(prompt))
+    # useful for debugging prompt size'
+    print(f"Prompt Lenght: {len(prompt)}")
 
-
+    #ask gemini to stream the response
     response = client.models.generate_content_stream(
         model="gemini-3.5-flash",
         contents=prompt
-    )    
-    
+
+
+    )
+
+    # yield each chunk as soon gemini generates it
+
     for chunk in response:
         if chunk.text:
-            yield chunk.text
+            yield chunk
